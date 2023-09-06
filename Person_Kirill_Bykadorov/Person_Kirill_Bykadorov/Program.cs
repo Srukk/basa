8d0f939de174ee4e038b1128d4dc301858b62377
Srukk <106874398+Srukk@users.noreply.github.com>
2023-09-06 18:04:18 +0300

    Add files via upload

---
+++ Person_Kirill_Bykadorov/Person_Kirill_Bykadorov/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Kirill_Bykadorov
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Кирилл";
+            string fam = "Быкадоров";
+            string surname = "Владимирович";
+            string group = "02ВТд4120";
+            string curs = "3 курс";
+            string id = "id: 29343916";
+            Console.WriteLine("Фамилия: " + fam + "Имя: " + name + "Отчество: " + surname + " " + "Группа: " + group +" "+ curs +" "+ id) ;
+        }
+    }
+}
